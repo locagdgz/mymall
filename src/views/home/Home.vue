51f91515b0e94e57ea9d08d91357b82372385439
mac <locagdgz@gmail.com>
2020-09-05 22:29:35 +0800

    添加轮播图

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,18 +3,22 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
+    <home-swiper :banner="banner"></home-swiper>
     <h2>首页</h2>
   </div>
 </template>
 
 <script>
     import NavBar from "components/common/navbar/NavBar";
+    import HomeSwiper from "./childComps/HomeSwiper";
+
     import {getHomeMultiData} from "network/home";
 
     export default {
         name: "Home",
         components: {
-          NavBar
+          NavBar,
+          HomeSwiper
         },
         data(){
           return {
