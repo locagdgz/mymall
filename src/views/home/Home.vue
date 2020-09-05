69a78291a6b38569c09572983f157dfed9570b7e
mac <locagdgz@gmail.com>
2020-09-05 22:07:53 +0800

    首页组件引入导航栏组件,添加首页数据 axios请求

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,45 @@
 <template>
-  <div>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
     <h2>首页</h2>
   </div>
 </template>
 
 <script>
+    import NavBar from "components/common/navbar/NavBar";
+    import {getHomeMultiData} from "network/home";
+
     export default {
-        name: "Home"
+        name: "Home",
+        components: {
+          NavBar
+        },
+        data(){
+          return {
+            banner: [],
+            dKeyword: [],
+            keywords: [],
+            recommend:[]
+          }
+        },
+        created() {
+          // 请求多个数据
+          getHomeMultiData().then(data => {
+            console.log(data);
+            this.banner = data.banner.list
+            this.dKeyword = data.dKeyword.list
+            this.keywords = data.keywords.list
+            this.recommend = data.recommend.list
+          })
+        }
     }
 </script>
 
 <style scoped>
-
+  .home-nav{
+    background-color: var(--color-tint);
+    color: #ffffff;
+  }
 </style>
